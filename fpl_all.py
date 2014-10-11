a8c238409721d6e58a68b20912c6c67897e80fd3
kirankaranth1 <kirankaranth1@gmail.com>
2014-10-11 10:40:06 +0530

    Added: fetch scores for past game weeks

--- fpl_all.py
+++ fpl_all.py
@@ -48,14 +48,14 @@ def get_team(team,gw):
     print("1. "+names[1]+" "+str(scores[1])+", "+names[2]+" "+str(scores[2])+", "+names[3]+" "+str(scores[3])+", "+names[4]+" "+str(scores[4])+" = "+str(scores[1]+scores[2]+scores[3]+scores[4]),file=f)
     print("2. "+names[0]+" "+str(scores[0])+"*2 = "+str(scores[0]*2),file=f)
     ha=0
-    if team[6]=='h':
+    if team[gw]=='h':
         
         ha=max(scores)*0.2
         print("3. Home advantage= "+"0.2*"+str(max(scores))+"="+str(ha),file=f)
     else:
         print("3. No home advantage",file=f)
         
-    print("Total="+str(scores[1]+scores[2]+scores[3]+scores[4]+scores[0]*2+ha),file=f)
+    print("Total="+str(scores[1]+scores[2]+scores[3]+scores[4]+scores[0]*2+ha)+" ~"+str(round(scores[1]+scores[2]+scores[3]+scores[4]+scores[0]*2+ha)),file=f)
     print("",file=f)
     
 
@@ -63,39 +63,40 @@ def get_team(team,gw):
 try:
     #game=raw_input("Home or Away(h/a): ")
     
-    gw=6
+    gw=7
     fn="Gameweek_"+str(gw)+".txt"
     f = open(fn,'a')
     #gw=input("Game week number: ")
-    hull=[192687,1677986,1149861,282609,235293,"Hull City","h"]
-    arsenal=[454989,1187227,379286,59139,1874374,"Arsenal FC","a"]
-    villa=[1511244,1793894,1245460,286331,870575,"Aston Villa","h"]
-    burnley=[108067,325056,684282,333912,136183,"Burnley","a"]
-    chelsea=[79759,1657614,2365314,320451,386880,"Chelsea","h"]
-    palace=[297679,344694,192858,72711,1594177,"Crystal Palace","a"]
-    everton=[1251421,2110064,995844,193410,451136,"Everton","a"]
-    leicester=[286373,2093448,257328,41975,1318470,"Leicester City","h"]
-    liverpool=[541519,833373,400106,468192,194237,"Liverpool FC","h"]
-    city=[721763,384500,540855,1620596,68930,"Manchester City","a"]
-    scums=[2326261,262415,51092,81179,1461413,"Manchester United","h"]
-    newcastle=[407205,342876,52355,56998,1144868,"Newcastle United","a"]
-    qpr=[277719,264939,1880769,1697834,1599192,"Queens Park Rangers","a"]
-    saints=[258535,383212,278413,2317698,1173922,"Southampton","a"]
-    stoke=[376009,2062491,779871,366861,451272,"Stoke City","a"]
-    ham=[1031829,319332,1485675,2639408,654398,"West Ham United","h"]
-    sunderland=[15003,129763,86905,385980,1203445,"Sunderland","h"]
-    swans=[1721201,1340663,82318,1276345,506284,"Swansea","h"]
-    spurs=[1046402,103596,901455,232054,1628118,"Tottenham","h"]
-    westbrom=[166582,310730,68335,276748,1392297,"West Brom","a"]    
+    hull=[192687,1677986,1149861,282609,235293,"Hull City","h","h","a"]
+    arsenal=[454989,1187227,379286,59139,1874374,"Arsenal FC","h","a","h"]
+    villa=[1511244,1793894,1245460,286331,870575,"Aston Villa","a","h","a"]
+    burnley=[108067,325056,684282,333912,136183,"Burnley","a","a","h"]
+    chelsea=[79759,1657614,2365314,320451,386880,"Chelsea","h","h","a"]
+    palace=[297679,344694,192858,72711,1594177,"Crystal Palace","h","a","h"]
+    everton=[1251421,1375942,995844,193410,451136,"Everton","a","a","h"]
+    leicester=[286373,2093448,257328,41975,1318470,"Leicester City","a","h","a"]
+    liverpool=[541519,833373,400106,468192,194237,"Liverpool FC","h","h","a"]
+    city=[721763,384500,540855,1620596,68930,"Manchester City","a","a","h"]
+    scums=[2326261,262415,51092,81179,1461413,"Manchester United","h","h","a"]
+    newcastle=[407205,342876,52355,56998,1144868,"Newcastle United","a","a","h"]
+    qpr=[277719,264939,1880769,1697834,1599192,"Queens Park Rangers","a","a","h"]
+    saints=[258535,383212,278413,2317698,1173922,"Southampton","h","a","h"]
+    stoke=[376009,2062491,779871,366861,451272,"Stoke City","h","a","h"]
+    ham=[1031829,319332,1485675,2639408,654398,"West Ham United","a","h","a"]
+    sunderland=[15003,129763,86905,385980,1203445,"Sunderland","h","h","a"]
+    swans=[1721201,1340663,82318,1276345,506284,"Swansea","a","h","a"]
+    spurs=[1046402,103596,901455,232054,1628118,"Tottenham","a","h","a"]
+    westbrom=[166582,310730,68335,276748,1392297,"West Brom","h","a","h"]    
     
     
-    get_team(hull,gw)
+    
     get_team(arsenal,gw)
     get_team(villa,gw)
     get_team(burnley,gw)
     get_team(chelsea,gw)
     get_team(palace,gw)
     get_team(everton,gw)
+    get_team(hull,gw)
     get_team(leicester,gw)
     get_team(liverpool,gw)
     get_team(city,gw)
